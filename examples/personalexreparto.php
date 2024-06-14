099a5b0f6981f4ebe8491fb359549fee1a823972
crlttdnt <carlotta.donato03@gmail.com>
2024-06-14 11:44:40 +0200

    personalexreparto finito

--- examples/personalexreparto.php
+++ examples/personalexreparto.php
@@ -20,12 +20,18 @@ if (isset($_SESSION['error_message'])) {
 $selectedReparto = null;
 $selectedCodice = null;
 if (isset($_POST['RepartoCodice'])) {
-    list($selectedReparto, $selectedCodice) = explode(',', $_POST['RepartoCodice']);
+    if ($_POST['RepartoCodice'] === "all") {
+        $selectedReparto = "Tutti i reparti";
+        $selectedCodice = "Tutti gli ospedali";
+    } else {
+        list($selectedReparto, $selectedCodice) = explode(',', $_POST['RepartoCodice']);
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <title>Gestore Aziende Ospedaliere</title>
@@ -33,67 +39,76 @@ if (isset($_POST['RepartoCodice'])) {
     <link rel="stylesheet" href="../css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-<header>
-    <div class="container-xl pt-3">
-        <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
-            <h2 class="m-0 fw-bold text-white">Gestione Aziende Ospedaliere</h2>
-            <a class="btn btn-outline-light" href="../login.php">
-                Login
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-                </svg>
-            </a>
-        </nav>
-    </div>
-</header>
-
-<section class="container-xl mx-auto my-5">
-    <div class="bg-white mx-auto p-5 rounded-4 fit-content border border-secondary border-10">
-        <h5 class='fw-medium'>Visualizza il personale organico nei reparti della struttura:</h5>
-        <form method="POST" class="w-100" action="personalexreparto.php">
-            <div class='d-flex justify-content-end align-items-center'>
-                <?php showSelect($selectedReparto, $selectedCodice); ?>
-                <button class="btn btn-secondary btn-lg ms-3 h-100" type="submit">Visualizza</button>
-            </div>
-        </form>
-    </div>
-</section>
-
-<section class="container-xl mx-auto my-5">
-    <div class="bg-white mx-auto p-5 rounded-4 fit-content border border-secondary border-4">
-        <div class='d-flex justify-content-between mb-5 align-items-center'>
-            <h3 class='fw-bold text-capitalize'><?php echo htmlspecialchars($table); ?></h3>
-            <a class='btn btn-outline-secondary me-4 fs-5' href="insert.php">Inserisci <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-arrow-right-short' viewBox='0 0 16 16'>
-                    <path fill-rule='evenodd' d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8'/>
-                </svg></a>
+    <header>
+        <div class="container-xl pt-3">
+            <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+                <h2 class="m-0 fw-bold text-white">Gestione Aziende Ospedaliere</h2>
+                <a class="btn btn-outline-light" href="../login.php">
+                    Login
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                    </svg>
+                </a>
+            </nav>
         </div>
-        <?php
-        if ($selectedReparto && $selectedCodice) {
-            showTable($selectedReparto, $selectedCodice);
-        }
-        ?>
-    </div>
-</section>
+    </header>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <section class="container-xl mx-auto my-5">
+        <div class="bg-white mx-auto p-5 rounded-4 fit-content border border-secondary border-10">
+            <h5 class='fw-medium'>Visualizza il personale organico nei reparti della struttura:</h5>
+            <form method="POST" class="w-100" action="personalexreparto.php">
+                <div class='d-flex justify-content-end align-items-center'>
+                    <?php showSelect($selectedReparto, $selectedCodice); ?>
+                    <button class="btn btn-secondary btn-lg ms-3 h-100" type="submit">Visualizza</button>
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <section class="container-xl mx-auto my-5">
+        <div class="bg-white mx-auto p-5 rounded-4 fit-content border border-secondary border-10">
+            <div class='d-flex justify-content-between align-items-center'>
+                <!--
+                <a class='btn btn-outline-secondary me-4 fs-5' href="insert.php">Inserisci <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-arrow-right-short' viewBox='0 0 16 16'>
+                        <path fill-rule='evenodd' d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8' />
+                    </svg></a>
+                -->
+            </div>
+            <?php
+            if ($selectedReparto && $selectedCodice) {
+                echo "<p>Personale del reparto " . htmlspecialchars($selectedReparto) . " dell'Ospedale " . htmlspecialchars($selectedCodice) . ":</p>";
+                showTable($selectedReparto, $selectedCodice);
+            }
+            ?>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
+
 </html>
 
+
+
+
 <?php
-function showTable($nomeReparto, $codice) {
+function showTable($nomeReparto, $codice)
+{
     $conn = connectToDatabase();
 
-    $query = "SELECT codiceFiscale, nome, cognome FROM Personale WHERE nomeReparto = $1 AND codice = $2";
-
-    try {
+    if ($nomeReparto === "Tutti i reparti" && $codice === "Tutti gli ospedali") {
+        $query = "SELECT codiceFiscale, nome, cognome, nomeReparto, codice FROM Personale";
+        $results = pg_query($conn, $query);
+    } else {
+        $query = "SELECT codiceFiscale, nome, cognome FROM Personale WHERE nomeReparto = $1 AND codice = $2";
         $results = pg_query_params($conn, $query, array($nomeReparto, $codice));
-        if (!$results) {
-            throw new Exception(pg_last_error($conn));
-        }
-    } catch (Exception $e) {
-        $_SESSION['error_message'] = $e->getMessage();
+    }
+
+    if (!$results) {
+        $_SESSION['error_message'] = pg_last_error($conn);
         header("Refresh:0");
         exit();
     }
@@ -117,11 +132,13 @@ function showTable($nomeReparto, $codice) {
     pg_close($conn);
 }
 
-function showSelect($selectedReparto, $selectedCodice) {
+function showSelect($selectedReparto, $selectedCodice)
+{
     $conn = connectToDatabase();
     $query = "SELECT DISTINCT nomeReparto, codice FROM Personale";
     $string = "<select class='form-select' name='RepartoCodice'>";
     $string .= "<option value='' disabled selected>Seleziona Reparto e Codice</option>";
+    $string .= "<option value='all'>Tutti i reparti</option>";
     $options = "";
 
     try {
