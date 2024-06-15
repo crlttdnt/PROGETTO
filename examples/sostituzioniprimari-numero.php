1f46755f7cbd57e03d923ba4f97566c053fc7393
crlttdnt <carlotta.donato03@gmail.com>
2024-06-15 10:47:44 +0200

    sostituzioni primario finitaaa

---
+++ examples/sostituzioniprimari-numero.php
@@ -0,0 +1,168 @@
+<?php
+session_start();
+include '../php/utility.php';
+
+if (isset($_GET["table"])) {
+    $table = strtolower($_GET['table']);
+    $_SESSION['table'] = $table;
+} elseif (isset($_SESSION['table'])) {
+    $table = strtolower($_SESSION['table']);
+} else {
+    $table = null;
+}
+
+if (isset($_SESSION['error_message'])) {
+    $error_message = $_SESSION['error_message'];
+    echo "<script type='text/javascript'>alert('$error_message');</script>";
+    unset($_SESSION['error_message']);
+}
+
+$selectedOption = null;
+
+if (isset($_POST['numSostituzioni'])) {
+    $selectedOption = $_POST['numSostituzioni'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Gestore Aziende Ospedaliere</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="stylesheet" href="../css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+<header>
+    <!-- Navigation -->
+    <div class="container-xl pt-3">
+        <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+            <h2 class="m-0 fw-bold text-white">Gestione Aziende Ospedaliere</h2>
+            <div>
+                <a class="btn btn-outline-light me-2" href="../php/view.php">
+                    Home
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+                        <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+                    </svg>
+                </a>
+                <a class="btn btn-outline-light" href="../login.php">
+                    Login
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                    </svg>
+                </a>
+            </div>
+        </nav>
+    </div>
+</header>
+
+<section class="container-xl mx-auto my-5">
+    <div class="bg-white mx-auto p-5 rounded-4 fit-content border border-secondary border-10">
+        <h5 class='fw-medium'>Visualizza VicePrimari per numero di sostituzioni:</h5>
+        <form method="POST" class="w-100" action="">
+            <div class='d-flex justify-content-end align-items-center'>
+                <?php showSelect($selectedOption); ?>
+                <button class="btn btn-secondary btn-lg ms-3 h-100" type="submit">Visualizza</button>
+            </div>
+        </form>
+    </div>
+</section>
+
+<section class="container-xl mx-auto my-5">
+    <div class="bg-white mx-auto p-5 rounded-4 fit-content border border-secondary border-10">
+        <?php
+        if ($selectedOption !== null) {
+            showTable($selectedOption);
+        }
+        ?>
+    </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+<?php
+function showTable($numSostituzioni)
+{
+    $conn = connectToDatabase();
+
+    switch ($numSostituzioni) {
+        case 'none':
+            $query = " SELECT codiceFiscale AS VicePrimario
+                        FROM VicePrimario
+                        EXCEPT
+                        SELECT VicePrimario
+                        FROM sostituisce;";
+            break;
+        case 'one':
+            $query = "SELECT VicePrimario FROM sostituisce 
+                      GROUP BY VicePrimario 
+                      HAVING COUNT(*) = 1";
+            break;
+        case 'two_or_more':
+            $query = "SELECT VicePrimario FROM sostituisce 
+                      GROUP BY VicePrimario 
+                      HAVING COUNT(*) >= 2";
+            break;
+        default:
+            $_SESSION['error_message'] = "Invalid selection.";
+            header("Refresh:0");
+            exit();
+    }
+
+    $results = pg_query($conn, $query);
+
+    if (!$results) {
+        $_SESSION['error_message'] = pg_last_error($conn);
+        header("Refresh:0");
+        exit();
+    }
+
+    $rowCount = pg_num_rows($results);
+
+    if ($rowCount > 0) {
+        echo "<table class='table'>";
+        echo "<thead><tr><th>VicePrimario</th></tr></thead>";
+        echo "<tbody>";
+        while ($row = pg_fetch_assoc($results)) {
+            echo "<tr>";
+            echo "<td>" . htmlspecialchars($row['viceprimario']) . "</td>";
+            echo "</tr>";
+        }
+        echo "</tbody></table>";
+    } else {
+        echo "<p>No results found.</p>";
+    }
+
+    pg_close($conn);
+}
+
+
+function showSelect($selectedOption)
+{
+    $options = [
+        'none' => 'Nemmeno una volta',
+        'one' => 'Una volta',
+        'two_or_more' => 'Due volte o più'
+    ];
+
+    $string = "<select class='form-select' name='numSostituzioni'>";
+    $string .= "<option value='' disabled selected>Seleziona numero di sostituzioni</option>";
+
+    foreach ($options as $value => $label) {
+        $isSelected = ($selectedOption === $value) ? " selected" : "";
+        $string .= "<option value='{$value}'{$isSelected}>{$label}</option>";
+    }
+
+    $string .= "</select>";
+
+    echo $string;
+}
+?>
--- php/view.php
+++ php/view.php
@@ -71,7 +71,7 @@ if (isset($_SESSION['error_message'])) {
                 echo "
                 <div class='d-flex justify-content-between align-items-center text-center mt-10' style='width: 100%;'>
                 <div>
-                    <h3 class='fw-bold fs-6 text-uppercase'> Personale per reparto </h3>
+                    <h3 class='fw-bold fs-6 text-uppercase'> Personale per Reparto </h3>
                     <a href='../examples/personalexreparto.php'>
                         <button class='btn btn-outline-dark' type='submit'>
                                 Vai alla pagina 
@@ -83,7 +83,7 @@ if (isset($_SESSION['error_message'])) {
                 </div>
                 
                 <div class='align-items-center'>
-                    <h3 class='fw-bold fs-6 text-uppercase'> Ricovero per paziente 
+                    <h3 class='fw-bold fs-6 text-uppercase'> Ricovero per Paziente 
                     </h3>
                     <a href='../examples/ricoveroxpaziente.php'>
                     <button class='btn btn-outline-dark' type='submit'>
@@ -96,9 +96,21 @@ if (isset($_SESSION['error_message'])) {
                 </div>
 
                 <div>
-                    <h3 class='fw-bold fs-6 text-uppercase'> Sostituzioni primari
+                    <h3 class='fw-bold fs-6 text-uppercase'> Sostituzioni Primari
                     </h3>
                     <a href='../examples/sostituzioniprimari.php'>
+                       <button class='btn btn-outline-dark' type='submit'>
+                            Vai alla pagina 
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-up-right' viewBox='0 0 16 16'>
+                                <path fill-rule='evenodd' d='M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z'/>
+                            </svg>  
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <h3 class='fw-bold fs-6 text-uppercase'> Numero Sostituzioni Primari
+                    </h3>
+                    <a href='../examples/sostituzioniprimari-numero.php'>
                        <button class='btn btn-outline-dark' type='submit'>
                             Vai alla pagina 
                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-up-right' viewBox='0 0 16 16'>
